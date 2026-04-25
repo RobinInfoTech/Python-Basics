0b7ed685479fff0de44470374ecc721b5ce838a6
RobinInfoTech <robinkurt2007@icloud.com>
2026-04-26 01:36:03 +0300

    Create addition_program.py

---
+++ Basic Programs/addition_program.py
@@ -0,0 +1,27 @@
+def addition(a,b):
+    '''
+    Function Name:
+    Function Description
+
+
+    Written by Robin :>
+    Copyright 2026
+    '''
+    c = a + b
+    return c
+
+
+
+
+print("Addition Program")
+# user enter inputs
+integer1 = int(input("Enter the first integer: "))
+integer2 = int(input("Enter the second integer: "))
+sum_of_ints = addition(integer1, integer2)
+
+
+# output
+print("Total sum of 2 integers:",sum_of_ints)
+
+
+
